dd680e593aae55207d03fc85000aaefd0fec047f
bgk <blakegraysonkehler@gmail.com>
2017-11-15 12:35:09 -0800

    Added comments to routes specfifying what should be returned

--- server/routes/playlists.js
+++ server/routes/playlists.js
@@ -8,26 +8,35 @@ const router = express.Router()
 module.exports = (DataHelpers) => {
   // Dashboard - guest
   router.get('/:cityId', (req, res) => {
-
+    // Display dashboard
+    // Return nothing
   })
 
   // Dashboard - logged in
   router.get('/:cityId/users/:id', (req, res) => {
+
+    // Return json playlists 'current', 'archived' for each location user has saved
   })
 
   // Add song to user's playlist
   router.post('/:cityId/users/:id', (req, res) => {
     const { songId, type } = req.body
+
+    // Return confirmation response
   })
 
   // Move song betweet user's playlist
   router.post('/:cityId/users/:id', (req, res) => {
     const { songId, type } = req.body
+
+    // Return confirmation response
   })
 
   // Remove single song from user's playlist
   router.delete('/:cityId/users/:id', (req, res) => {
     const { songId, type } = req.body
+
+    // Remove confirmation response
   })
 
   return router
--- server/routes/users.js
+++ server/routes/users.js
@@ -8,30 +8,40 @@ module.exports = (DataHelpers) => {
   // Login
   router.put('/', (req, res) => {
     
+    // Return confirmation response
+    // Redirect to dashboard
   })
   // Register
   router.post('/', (req, res) => {
     
+    // Return confrimation response
+    // Redirect to dashboard
   })
 
   // Logout
   router.delete('/', (req, res) => {
-    
+
+    // Return confirmation response
+    // Redirect to landing
   })
 
   // User profile
   router.get('/:id', (req, res) => {
 
+    // Return user as json file
   })
 
   // Edit profile
   router.put('/:id', (req, res) => {
     
+    // Return confirmation response
   })
 
   // Delete account
   router.delete('/:id', (req, res) => {
     
+    // Return confirmation response
+    // Redirect to landing
   })
 
   return router
