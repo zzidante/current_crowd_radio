e86f7b40a7a56012e30648ec62a61995cac528b0
Mel <zzidante@gmail.com>
2017-11-16 21:53:46 +0000

    fiddling with classes

--- client/src/Player/Tracklist.jsx
+++ client/src/Player/Tracklist.jsx
@@ -4,19 +4,18 @@ class Tracklist extends Component {
   constructor(props) {
     super(props);
   }
-
   setTrack = event => {
     this.props.selectTrack(this.props.index)
   }
   render() {
     const {id, trackHREF, name, artist, album, image, duration} = this.props.track
     return (
-      <div className="track">
-        <img src={image} />
-        <span>{artist}</span> 
-        <span>{album}</span>
-        <span><Duration seconds = {duration} /> </span>
-        <span><button onClick={this.setTrack}><i className="fa fa-play"></i></button></span>
+      <div className="track-container">
+        <img className= "track-art" src={image} />
+        <span className="track-artist">{artist}</span> 
+        <span className="track-album">{album}</span>
+        <span className="track-duration"><Duration seconds = {duration} /> </span>
+        <span className="track-play-btn"><button onClick={this.setTrack}><i className="fa fa-play"></i></button></span>
       </div>
   )
   }
