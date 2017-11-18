8a92fe81e505b292b4cae2cacb160be5dbc55973
Mel <zzidante@gmail.com>
2017-11-18 02:42:16 +0000

    Fixed button side after merge erro

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,21 +8,7 @@ import './styles/css/index.css';
 class App extends Component {
   constructor(props) {
     super(props);
-<<<<<<< HEAD
-    this.state = {
-      tracklist: [],
-      location: {
-        country: "",
-        city: ""
-      },
-      type: "",
-      user: ""
-    };
-    // this.getTracks();   // use this to get the tracks
-    this.APIKEY = "b48755b6";
-=======
     this.APIKEY = 'b48755b6';
->>>>>>> feature/react-router
   }
   render() {
     return (
--- client/src/Nav.jsx
+++ client/src/Nav.jsx
@@ -76,7 +76,7 @@ class Nav extends Component {
        
         <form onSubmit={this.setLocation}>
         <PlacesAutocomplete id="location-search" inputProps={inputProps} styles={myStyles}/>
-        <button type="submit">Submit</button>
+        <button type="submit main-btn">Submit</button>
       </form>
         <nav>
           <li>
