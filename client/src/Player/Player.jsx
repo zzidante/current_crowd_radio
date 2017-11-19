0a18e74eed4fa920c775a35d5ed67447d605a3a7
Mel <zzidante@gmail.com>
2017-11-19 22:55:27 +0000

    added conditional rendering to full-playlist so border doesn't appear when no playlist exists

--- client/src/Player/Player.jsx
+++ client/src/Player/Player.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Controls from './Controls.jsx'
 import Tracklist from './Tracklist.jsx'
 class Player extends Component {
+  
   render() {
     const trackArray = window.getState().tracklist.map( (track, i) => {
       return (
@@ -11,7 +12,7 @@ class Player extends Component {
     return (
         <section className="music-container col-md-6 col-md-pull-3 col-xs-12">
           <Controls key={"controls"} currentTrack={window.getState().tracklist[window.getState().currentTrackIndex]}/>
-          <section className="full-playlist">
+          <section className={trackArray.length > 0 ? "full-playlist add-border" : "full-playlist"}>
             <div className="track-container">
               {trackArray}
             </div>
