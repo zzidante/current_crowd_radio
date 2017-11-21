0d38c2a079062e37cbfe3f4baf85471d300027fd
Curtis Bateson <curtislbateson@gmail.com>
2017-11-21 19:28:35 +0000

    Modified buildpacks to allow use of REACT_APP vars v2

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import iso from "iso-3166-1";
 
-// Converts text to city/county codes, sets state, and loads new tracklist. :)
+// Converts text to city/county codes, sets state, and loads new tracklist.
 const setLocation = () => {
   const loc = window.getState().locationBar;
   const city = loc.match(/^\w+[a-z]?/i)[0];
