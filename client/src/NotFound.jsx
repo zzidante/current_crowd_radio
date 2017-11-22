8aba227c63da1497dd3b66377d37da0836fcf330
bgk <blakegraysonkehler@gmail.com>
2017-11-21 16:07:06 -0800

    Added notfound.jsx and panda png

---
+++ client/src/NotFound.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import panda from './images/sad_panda.png'
+
+function NotFound () {
+  const divStyle = {
+    backgroundcolor: '0E2E39',
+    margin: "auto",
+    width: "100vw"
+  }
+  const imgStyle = {
+    maxwidth: "100vw"
+  }
+  return (
+    <div style={divStyle}>
+      <h1>
+        <p>404</p>        
+      </h1>
+      <img src={panda} alt="sorry..." height="375" width="375" style={imgStyle}/>
+    </div>
+  )
+}
+
+export default NotFound
