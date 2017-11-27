92e1de5b2157e3084f7bd6228e3516480990e219
Mel <zzidante@gmail.com>
2017-11-27 06:30:39 +0000

    Added small brand image into component directly

--- client/src/NavMenu/BrandDeclaration.jsx
+++ client/src/NavMenu/BrandDeclaration.jsx
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
-
+import miniLogo from "../images/small_logo_lt_gray.png"
+// mini-brand-icon
 class BrandDeclaration extends Component {
   
   render() {
+    const imgStyle = {
+      height: '40px',
+      width: '40px',
+      marginTop: '3px',
+      margin: '0 5px 0 0',
+    };
+
     return (
-      <h3>
-        <img id="mini-brand-icon" src="https://www.honoryoga.com/wp-content/uploads/2016/05/icon-50x50.png" />
+      <div className="mini-brand-container">
+        <img
+        src={miniLogo}
+        alt="Radio Logo"
+        style={imgStyle}
+      />
         <a href="#">Current Crowd Radio</a>
-      </h3>
+      </div>
     )
   }
 }
